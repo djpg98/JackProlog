f64c3576c6e3a077497289621d53ab1fb53a51a4
Diego <djpg98@gmail.com>
2020-03-31 20:45:31 -0400

    Started with the pawns

--- ajedrez.pl
+++ ajedrez.pl
@@ -159,6 +159,19 @@ moverAlfil(Jugador, [X, Y], Desp, Anterior, Actual) :- X2 is X + Desp, Y2 is Y +
                                                        libre(Jugador, X2, Y2, Anterior, NuevoTablero, Estado),
                                                        moverDDI(Jugador, [X, Y], [X2, Y2], Desp, Estado, NuevoTablero, Actual).
 
+moverPeon(Jugador, [X, Y], Anterior, Actual) :- Jugador = blancas, X = 7, X2 is X - 2, 
+                                                peonAvanza(Jugador, [X, Y], [X2, Y], Anterior, Actual).
+moverPeon(Jugador, [X, Y], Anterior, Actual) :- Jugador = negras, X = 2, X2 is X + 2, 
+                                                peonAvanza(Jugador, [X, Y], [X2, Y], Anterior, Actual).
+moverPeon(Jugador, [X, Y], Anterior, Actual) :- Jugador = blancas, X = 7, X2 is X - 1, 
+                                                peonAvanza(Jugador, [X, Y], [X2, Y], Anterior, Actual).
+moverPeon(Jugador, [X, Y], Anterior, Actual) :- Jugador = negras, X = 2, X2 is X + 1, 
+                                                peonAvanza(Jugador, [X, Y], [X2, Y], Anterior, Actual).
+
+peonAvanza(Jugador, [X, Y], [X2, Y2], Anterior, Actual) :- coordenadas(X2, Y2),
+                                                           libre(Jugador, X2, Y2, Anterior, NuevoTablero, Estado),
+                                                           Estado = 1, actualizarTablero(X, Y, X2, Y, NuevoTablero, Actual).
+
 moverPieza(Jugador, [X, Y], Movimientos, Anterior, Actual) :- getElement(Movimientos, [DespX, DespY]), 
                                                               moverAux(Jugador, [X, Y], [DespX, DespY], Anterior, Actual).
 
@@ -255,4 +268,4 @@ caballo(negras,4,6),rey(blancas,8,5),peon(blancas,7,5),dama(blancas,6,4), torre(
 moverAlfil(blancas, [5, 5], 1, [alfil(blancas, 5, 5), torre(negras,1,1),peon(negras,2,2),peon(negras,2,4),rey(negras,1,4),
 caballo(negras,4,6),rey(blancas,8,5)], Actual).
 
-moverPieza(negras, [4,4], [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]], [caballo(negras, 5, 5)], Actual) */
+moverPieza(negras, [4,4], [[-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1]], [caballo(negras, 4, 4)], Actual) */
